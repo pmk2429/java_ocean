4cfe3b1c0e0b9365fb81de09d61b67ed6a0a7457
pmk2429 <pmk.2429@gmail.com>
2015-05-09 11:15:26 -0700

    singleton pattern

---
+++ src/designPattern/singletonPattern/SinglePatternDemo.java
@@ -0,0 +1,16 @@
+package designPattern.singletonPattern;
+
+public class SinglePatternDemo {
+	public static void main(String[] args) {
+
+		// illegal construct
+		// Compile Time Error: The constructor SingleObject() is not visible
+		// SingleObject object = new SingleObject();
+
+		// Get the only object available
+		SingleObject object = SingleObject.getInstance();
+
+		// show the message
+		object.showMessage();
+	}
+}
