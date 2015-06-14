fbd8c6f41866cd57937a7344a9546669535d031d
Pavitra <pmk.2429@gmail.com>
2015-06-13 20:17:18 -0700

    design patterns

--- src/designPattern/compositePattern/CompositePatternDemo.java
+++ src/designPattern/compositePattern/CompositePatternDemo.java
@@ -1,5 +1,19 @@
 package designPattern.compositePattern;
 
+/**
+ * Composite pattern is used where we need to treat a group of objects in
+ * similar way as a single object. Composite pattern composes objects in term of
+ * a tree structure to represent part as well as whole hierarchy. This type of
+ * design pattern comes under structural pattern as this pattern creates a tree
+ * structure of group of objects.
+ * 
+ * This pattern creates a class that contains group of its own objects. This
+ * class provides ways to modify its group of same objects.
+ * 
+ * @author Pavitra
+ *
+ */
+
 public class CompositePatternDemo {
 	public static void main(String[] args) {
 
