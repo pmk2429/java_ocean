3acc15b9105a54d20c95bb97f1dd0a05a336aa2b
Pavitra <pmk.2429@gmail.com>
2015-09-15 20:24:46 -0400

    strategy design pattern

---
+++ src/designPattern/strategyPattern/OperationMultiply.java
@@ -0,0 +1,10 @@
+package designPattern.strategyPattern;
+
+public class OperationMultiply implements Strategy{
+
+	@Override
+	public int doOperation(int num1, int num2) {
+		return num1*num2;
+	}
+
+}
---
+++ src/designPattern/strategyPattern/OperationSubstract.java
@@ -0,0 +1,10 @@
+package designPattern.strategyPattern;
+
+public class OperationSubstract implements Strategy {
+
+	@Override
+	public int doOperation(int num1, int num2) {
+		return num1 - num2;
+	}
+
+}
